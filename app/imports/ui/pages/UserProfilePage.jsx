51ee4765a2d340877f9b62f93327572be235cead
zaxer2 <william.johns3@gmail.com>
2018-11-13 13:12:36 -1000

    UserProfilePage & demo style- WIP

--- app/imports/ui/pages/UserProfilePage.jsx
+++ app/imports/ui/pages/UserProfilePage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid, Icon, Button, Image, Container } from 'semantic-ui-react';
+import { Grid, Icon, Button, Image, Container, Card } from 'semantic-ui-react';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
@@ -20,26 +20,44 @@ class Landing extends React.Component {
               </Grid.Column>
               <Grid.Column width={5}>
               </Grid.Column>
-              <Grid.Column width={5}>
-                <Icon inverted className='landing-icon' name='chat' size='huge'></Icon>
-                <Icon inverted className='landing-icon' name='circle' size='huge'></Icon>
+              <Grid.Column width={5} textAlign='right'>
+                <Icon inverted className='landing-icon' name='chat' size='big'></Icon>
+                <Icon inverted className='landing-icon' name='circle' size='big'></Icon>
               </Grid.Column>
             </Grid.Row>
             <Grid.Row>
               <Grid.Column width={3}>
-                <p className = 'name'>
-                  Hi Steven,
+                <p className = 'profile-body'>
+                  Profile
                 </p>
+                <Button fluid>New Post</Button>
+                <Button fluid>Contact Admin</Button>
+                <Button fluid>Other Option</Button>
               </Grid.Column>
               <Grid.Column width={4}>
+                <p className = 'profile-body'>
+                  Past Meetups
+                </p>
+                <Card>
+                  <Card.Header>Example Listing</Card.Header>
+                  <Card.Meta>Example Owner</Card.Meta>
+                  <Card.Description>Example decription</Card.Description>
+                </Card>
               </Grid.Column>
               <Grid.Column width={4}>
-                <Icon inverted className='landing-icon' name='chat' size='huge'></Icon>
-                <Icon inverted className='landing-icon' name='circle' size='huge'></Icon>
+                <p className = 'profile-body'>
+                  Your Posts
+                </p>
+                <Card>
+                  <Card.Header>Example Listing</Card.Header>
+                  <Card.Meta>Example Owner</Card.Meta>
+                  <Card.Description>Example decription</Card.Description>
+                </Card>
               </Grid.Column>
-              <Grid.Column width={4}>
-                <Icon inverted className='landing-icon' name='chat' size='huge'></Icon>
-                <Icon inverted className='landing-icon' name='circle' size='huge'></Icon>
+              <Grid.Column width={4} textAlign='center'>
+                <Button fluid>New Post</Button>
+                <Button fluid>Contact Admin</Button>
+                <Button fluid>Other Option</Button>
               </Grid.Column>
 
             </Grid.Row>
